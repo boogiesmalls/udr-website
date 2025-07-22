74d4f14e79e0d1bbfbc83228975aff126165cf35
Edlando Eliacin <landoeliacin@gmail.com>
2025-07-22 09:28:06 -0400

    adjust spacing for page route

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -55,7 +55,7 @@ export default async function Page({ params: paramsPromise }: Args) {
   const { layout } = page
 
   return (
-    <article className="">
+    <article>
       {/* Allows redirects for valid pages too */}
       <PayloadRedirects disableNotFound url={url} />
 
